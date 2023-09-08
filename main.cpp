8ca0f68bc99fcb84e6875c8e2bb1e4e63ea6a006
ProDCG <mason.stuart05@outlook.com>
2023-09-07 17:55:16 -0700

    Added basic calculations. need to implement the rest of the constraints and physics in, along with the rendering component of the engine.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
 #include <SFML/Graphics.hpp>
 
+struct Obj {
+    sf::Vector2f last_position;
+    sf::Vector2f position;
+    sf::Vector2f acceleration;
+    float radius = 10.0f;
+    sf::Color color = sf::Color::White;
+
+    Obj() = default;
+    Obj(sf::Vector2f position_, float radius_)
+        :position{ position_ }, last_position(position_), acceleration{0.0f, 0.0f}, radius{radius_} {}
+
+    void update(double dt) {}
+
+    void accelerate(sf::Vector2f a) {
+        acceleration += a;
+    }
+
+    void setVelocity(sf::Vector2f v, float dt) {
+        last_position = position - (v * dt);
+    }
+
+    void addVelocity(sf::Vector2f v, float dt) {
+        last_position -= (v * dt);
+    }
+
+    [[nodiscard]]
+    sf::Vector2f getPosition() const {
+        return position;
+    }
+
+    [[nodiscard]]
+    sf::Vector2f getVelocity(float dt) const {
+        return (position - last_position) / dt;
+    }
+};
+
+class Solver {
+    public:
+        Solver() = default;
+
+
+
+    private:
+
+
+        void applyGravity() {}
+        void checkCollisions(double dt) {}
+        void applyConstraints(double dt) {}
+        void updateObjects(double dt) {}
+};
+
 int main() {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
